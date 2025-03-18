b3433b22496d8ecf0a1493d85bdfee22af2a757c
diya <diyarranaware1001@gmail.com>
2025-03-18 15:40:34 +0530

    initial commit

---
+++ 5.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int x, y;
+	cout<<"\n enter the value of x & y";
+	cin>>x>>y;
+	if(x>y)
+	cout<<x<<" is largest no.";
+	else
+	cout<<y<<" is largest no.";
+}
